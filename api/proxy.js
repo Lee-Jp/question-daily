7cc590af171bb99f57897b241fb42a9d411a91cd
李佳朋 <lijiapeng@chunyu.me>
2021-02-19 19:00:31 +0800

    init

---
+++ api/proxy.js
@@ -0,0 +1,26 @@
+/*
+ * @Description: 
+ * @Date: 2021-02-19 18:57:07
+ * @LastEditTime: 2021-02-19 18:59:00
+ * @FilePath: /question-daily/api/proxy.js
+ * @LastEditors: LiJiapeng
+ */
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  if (req.url.startsWith('/api')) {
+    target = 'https://biztest.chunyutianxia.com/'
+  }
+
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/'
+    }
+  })(req, res)
+}
