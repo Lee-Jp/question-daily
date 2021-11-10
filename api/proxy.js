6c38ec9fd9635a16b4e8807e2da40c0119ded2ba
李佳朋 <lijiapeng@chunyu.me>
2021-11-10 17:22:17 +0800

    init

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 /*
  * @Description: 
  * @Date: 2021-02-19 18:57:07
- * @LastEditTime: 2021-02-19 19:20:47
+ * @LastEditTime: 2021-11-10 17:21:28
  * @FilePath: /question-daily/api/proxy.js
  * @LastEditors: LiJiapeng
  */
@@ -10,8 +10,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
 
-  if (req.url.startsWith('/api')) {
+  if (req.url.startsWith('/cy/api')) {
     target = 'https://biztest.chunyutianxia.com'
+  }
+
+  if (req.url.startsWith('/cy/devops')) {
+    target = 'https://devops.chunyutianxia.com'
   }
 
   // 创建代理对象并转发请求
@@ -19,7 +23,13 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/api/': '/'
+      '^/cy/api/': '/'
+    }
+  },{
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/cy/devops/': '/'
     }
   })(req, res)
 }
